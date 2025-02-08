e20e451c9de0ecdfd6f5b25be591a0ff5c00083a
Facundo Otero <facundo_otero@hotmail.com>
2025-02-08 18:26:22 -0300

    prueba18

--- TPFinal/Infraestructura/terraform servicios/08-kubernetes.tf
+++ TPFinal/Infraestructura/terraform servicios/08-kubernetes.tf
@@ -4,8 +4,8 @@ resource "google_container_cluster" "cluster-integrador" {
   location                 = "us-east1-b"
   remove_default_node_pool = true
   initial_node_count       = 1
-  # network                  = google_compute_network.main.self_link
-  # subnetwork               = google_compute_subnetwork.private.self_link
+  network                  = google_compute_network.main.self_link
+  subnetwork               = google_compute_subnetwork.private.self_link
   logging_service          = "logging.googleapis.com/kubernetes"
   monitoring_service       = "monitoring.googleapis.com/kubernetes"
   networking_mode          = "VPC_NATIVE"
