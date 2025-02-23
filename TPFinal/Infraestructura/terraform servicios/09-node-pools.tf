dfece026bc0513dcafc9b4714ccf89cf319bec32
Facundo Otero <facundo_otero@hotmail.com>
2025-02-23 18:52:21 -0300

    45

--- TPFinal/Infraestructura/terraform servicios/09-node-pools.tf
+++ TPFinal/Infraestructura/terraform servicios/09-node-pools.tf
@@ -20,7 +20,7 @@ resource "google_container_node_pool" "general" {
 
   node_config {
     preemptible  = false
-    machine_type = "e2-standard-2"
+    machine_type = "e2-standard-4"
 
     labels = {
       role = "general"
@@ -50,7 +50,7 @@ resource "google_container_node_pool" "spot" {
 
   node_config {
     preemptible  = true
-    machine_type = "e2-standard-2"
+    machine_type = "e2-standard-4"
 
     labels = {
       team = "devops"
