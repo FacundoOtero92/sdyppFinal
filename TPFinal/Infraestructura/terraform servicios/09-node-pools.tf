5c26e7e33675ea0f3d873088d3f08814d22a724f
Facundo Otero <facundo_otero@hotmail.com>
2025-08-01 22:06:00 -0300

    26

--- TPFinal/Infraestructura/terraform servicios/09-node-pools.tf
+++ TPFinal/Infraestructura/terraform servicios/09-node-pools.tf
@@ -3,71 +3,7 @@
    account_id = "kubernetes"
  }
 
-# resource "google_container_node_pool" "general" {
-#   name       = "general"
-#   cluster    = google_container_cluster.cluster-integrador.id
-#   node_count = 0
 
-#   management {
-#     auto_repair  = true
-#     auto_upgrade = true
-#   }
-
-#   autoscaling {
-#     min_node_count = 1
-#     max_node_count = 10
-#   }
-
-#   node_config {
-#     preemptible  = false
-#     machine_type = "e2-standard-4"
-
-#     labels = {
-#       role = "general"
-#     }
-
-#     service_account = google_service_account.kubernetes.email
-#     oauth_scopes = [
-#       "https://www.googleapis.com/auth/cloud-platform"
-#     ]
-#   }
-# }
-
-# resource "google_container_node_pool" "spot" {
-#   name       = "spot"
-#   cluster    = google_container_cluster.cluster-integrador.id
-#   node_count = 2
-
-#   management {
-#     auto_repair  = true
-#     auto_upgrade = true
-#   }
-
-#   autoscaling {
-#     min_node_count = 2
-#     max_node_count = 10
-#   }
-
-#   node_config {
-#     preemptible  = true
-#     machine_type = "e2-standard-4"
-
-#     labels = {
-#       team = "devops"
-#     }
-
-#     taint {
-#       key    = "instance_type"
-#       value  = "spot"
-#       effect = "NO_SCHEDULE"
-#     }
-
-#     service_account = google_service_account.kubernetes.email
-#     oauth_scopes = [
-#       "https://www.googleapis.com/auth/cloud-platform"
-#     ]
-#   }
-# }
 # Node Pool para aplicaciones (ej: coordinador, workers)
 resource "google_container_node_pool" "apps_pool" {
   name       = "apps-pool"
@@ -78,7 +14,7 @@ resource "google_container_node_pool" "apps_pool" {
   autoscaling {
     min_node_count = 1
     max_node_count = 4
-    
+
   }
 
   management {
