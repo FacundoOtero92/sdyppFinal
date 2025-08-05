71267448764f793341db2ddafcbccd48ba03e4e3
Facundo Otero <facundo_otero@hotmail.com>
2025-08-05 17:42:06 -0300

    34

--- TPFinal/Infraestructura/terraform servicios/08-kubernetes.tf
+++ TPFinal/Infraestructura/terraform servicios/08-kubernetes.tf
@@ -3,7 +3,7 @@ resource "google_container_cluster" "cluster-integrador" {
   name                     = "cluster-integrador"
   location                 = "us-east1-c"
   remove_default_node_pool = true
-  initial_node_count       = 1
+  # initial_node_count       = 1
   network                  = google_compute_network.main.self_link
   subnetwork               = google_compute_subnetwork.private.self_link
   logging_service          = "logging.googleapis.com/kubernetes"
