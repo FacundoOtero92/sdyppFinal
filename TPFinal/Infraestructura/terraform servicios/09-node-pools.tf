eb981aa94c4c73bca4435a4d1ec9f7454b86a430
Facundo Otero <facundo_otero@hotmail.com>
2025-08-09 21:17:42 -0300

    37

--- TPFinal/Infraestructura/terraform servicios/09-node-pools.tf
+++ TPFinal/Infraestructura/terraform servicios/09-node-pools.tf
@@ -1,62 +1,70 @@
 
- resource "google_service_account" "kubernetes" {
-   account_id = "kubernetes"
- }
+resource "google_service_account" "kubernetes" {
+  account_id = "kubernetes"
+}
 
-
-# Node Pool para aplicaciones (ej: coordinador, workers)
-resource "google_container_node_pool" "apps_pool" {
-  name       = "apps-pool"
+resource "google_container_node_pool" "general" {
+  name       = "general"
   cluster    = google_container_cluster.cluster-integrador.id
-  location   = var.region
-  node_count = 1
-  depends_on = [google_container_cluster.cluster-integrador]
-
-  autoscaling {
-    min_node_count = 1
-    max_node_count = 4
-
-  }
+  node_count = 0
 
   management {
     auto_repair  = true
     auto_upgrade = true
   }
 
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 10
+  }
+
   node_config {
+    preemptible  = false
     machine_type = "e2-standard-4"
+
     labels = {
-      type = "aplicacion"
+      role = "general"
     }
+
     service_account = google_service_account.kubernetes.email
-    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
   }
 }
 
-# Node Pool para servicios (ej: Redis, RabbitMQ)
-resource "google_container_node_pool" "services_pool" {
-  name       = "services-pool"
+resource "google_container_node_pool" "spot" {
+  name       = "spot"
   cluster    = google_container_cluster.cluster-integrador.id
-  location   = var.region
-  node_count = 1
-  depends_on = [google_container_cluster.cluster-integrador]
-
-  autoscaling {
-    min_node_count = 1
-    max_node_count = 4
-  }
+  node_count = 2
 
   management {
     auto_repair  = true
     auto_upgrade = true
   }
 
+  autoscaling {
+    min_node_count = 2
+    max_node_count = 10
+  }
+
   node_config {
-    machine_type = "e2-medium"
+    preemptible  = true
+    machine_type = "e2-standard-4"
+
     labels = {
-       type = "servicio"
+      team = "devops"
     }
+
+    taint {
+      key    = "instance_type"
+      value  = "spot"
+      effect = "NO_SCHEDULE"
+    }
+
     service_account = google_service_account.kubernetes.email
-    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
   }
 }
