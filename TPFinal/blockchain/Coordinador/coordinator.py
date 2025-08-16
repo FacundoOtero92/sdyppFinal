d2972a101ac58e78f21eb82ad054bd9e7ec144fd
Facundo Otero <facundo_otero@hotmail.com>
2025-08-16 19:56:38 -0300

    44

--- TPFinal/blockchain/Coordinador/coordinator.py
+++ TPFinal/blockchain/Coordinador/coordinator.py
@@ -19,7 +19,7 @@ queueNameTx = 'QueueTransactions'
 exchangeBlock = 'ExchangeBlock'
 timer = 15
 datosBucket = []
-bucketName = 'bucket_integrador_final2025'
+bucketName = 'bucket_integrador_final082025'
 credentialPath = 'credentials.json'
 
 
